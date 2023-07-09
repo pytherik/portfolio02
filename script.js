b7fabecc59ecc6cfe839a26968da1f88d5729df2
pytherik <erikberndt@gmx.net>
2023-07-09 17:45:17 +0200

    make github buttons work

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const toggle = document.getElementById('toggle');
 const nav = document.getElementById('nav');
 const projects = document.querySelector('.img-container');
+
 toggle.addEventListener('click', () => nav.classList.toggle('active'));
-// nav.addEventListener('click', () => nav.classList.toggle('active'));
+nav.addEventListener('click', () => nav.classList.toggle('active'));
 
 projects.addEventListener('click', (e) => {
   if (!e.target.classList.contains('project') &&
